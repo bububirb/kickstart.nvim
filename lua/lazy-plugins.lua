e8ff1c014805c5d7431b2d318884d557cea70fdd
Bubu <73459333+bububirb@users.noreply.github.com>
2026-01-28 13:29:14 +0800

    feat: add nvim-dap plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -34,6 +34,7 @@ require('lazy').setup({
   require 'custom.plugins.mini',
   require 'custom.plugins.treesitter',
   require 'custom.plugins.cord',
+  require 'custom.plugins.nvim-dap',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
